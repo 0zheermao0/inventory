233f94f075b592eec64f3bd3845668d0e86beac7
Joey Neko <zengjiayi666@gmail.com>
2025-08-15 00:06:39 +0800

    修复CustomerList.vue中的模板语法

--- frontend/src/views/CustomerList.vue
+++ frontend/src/views/CustomerList.vue
@@ -4,7 +4,21 @@
       <template #header>
         <div class="clearfix">
           <span>客户管理</span>
-          <el-button type="primary" style="float: right;" @click="openCustomerForm()">新增客户</el-button>
+          <div style="float: right;">
+            <el-dropdown @command="handleCustomerDropdownCommand" style="margin-right: 10px;">
+              <el-button type="primary">
+                Excel操作<i class="el-icon-arrow-down el-icon--right"></i>
+              </el-button>
+              <template #dropdown>
+                <el-dropdown-menu>
+                  <el-dropdown-item command="export">导出客户</el-dropdown-item>
+                  <el-dropdown-item command="import">导入客户</el-dropdown-item>
+                  <el-dropdown-item command="download-template">下载模板</el-dropdown-item>
+                </el-dropdown-menu>
+              </template>
+            </el-dropdown>
+            <el-button type="primary" @click="openCustomerForm()">新增客户</el-button>
+          </div>
         </div>
       </template>
       
@@ -66,6 +80,31 @@
         </span>
       </template>
     </el-dialog>
+    
+    <!-- 导入客户对话框 -->
+    <el-dialog v-model="importCustomersDialogVisible" title="导入客户" width="500px">
+      <el-form label-width="100px">
+        <el-form-item label="选择Excel文件">
+          <el-upload
+            class="upload-demo"
+            action="#"
+            :auto-upload="false"
+            :show-file-list="true"
+            :on-change="handleCustomerFileChange"
+            accept=".xlsx,.xls"
+          >
+            <el-button size="small" type="primary">选择文件</el-button>
+          </el-upload>
+        </el-form-item>
+      </el-form>
+      
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="importCustomersDialogVisible = false">取消</el-button>
+          <el-button type="primary" @click="importCustomers" :loading="importing">导入</el-button>
+        </span>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
@@ -80,6 +119,9 @@ const dialogVisible = ref(false)
 const editingCustomer = ref(null)
 const submitting = ref(false)
 const customerFormRef = ref()
+const importing = ref(false)
+const importCustomersDialogVisible = ref(false)
+const selectedCustomerFile = ref(null)
 
 // 分页相关
 const total = ref(0)
@@ -201,5 +243,101 @@ const deleteCustomer = (customer) => {
   }).catch(() => {
     // 用户取消删除
   })
+}
+
+const handleCustomerDropdownCommand = (command) => {
+  switch (command) {
+    case 'export':
+      exportCustomers()
+      break
+    case 'import':
+      importCustomersDialogVisible.value = true
+      break
+    case 'download-template':
+      downloadCustomerTemplate()
+      break
+  }
+}
+
+const exportCustomers = async () => {
+  try {
+    const response = await axios.get('http://localhost:8000/api/inventory-transactions/export_customers/', {
+      responseType: 'blob'
+    })
+    
+    // 创建blob URL并触发下载
+    const url = window.URL.createObjectURL(new Blob([response.data]))
+    const link = document.createElement('a')
+    link.href = url
+    link.setAttribute('download', 'customers.xlsx')
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+    
+    ElMessage.success('客户资料导出成功！')
+  } catch (error) {
+    ElMessage.error('导出失败：' + error.message)
+  }
+}
+
+const downloadCustomerTemplate = async () => {
+  try {
+    const response = await axios.get('http://localhost:8000/api/inventory-transactions/download_customer_template/', {
+      responseType: 'blob'
+    })
+    
+    // 创建blob URL并触发下载
+    const url = window.URL.createObjectURL(new Blob([response.data]))
+    const link = document.createElement('a')
+    link.href = url
+    link.setAttribute('download', 'customers_template.xlsx')
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+    
+    ElMessage.success('客户资料模板下载成功！')
+  } catch (error) {
+    ElMessage.error('下载失败：' + error.message)
+  }
+}
+
+const handleCustomerFileChange = (file) => {
+  selectedCustomerFile.value = file.raw
+}
+
+const importCustomers = async () => {
+  if (!selectedCustomerFile.value) {
+    ElMessage.warning('请先选择文件')
+    return
+  }
+  
+  try {
+    importing.value = true
+    
+    const formData = new FormData()
+    formData.append('file', selectedCustomerFile.value)
+    
+    const response = await axios.post('http://localhost:8000/api/inventory-transactions/import_customers/', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    })
+    
+    ElMessage.success(`导入成功：新增${response.data.created_count}条，更新${response.data.updated_count}条`)
+    if (response.data.errors.length > 0) {
+      ElMessage.warning(`部分数据导入失败：${response.data.errors.join('; ')}`)
+    }
+    
+    importCustomersDialogVisible.value = false
+    fetchCustomers()
+  } catch (error) {
+    if (error.response && error.response.data) {
+      ElMessage.error('导入失败：' + error.response.data.error)
+    } else {
+      ElMessage.error('导入失败：' + error.message)
+    }
+  } finally {
+    importing.value = false
+  }
 }
 </script>
